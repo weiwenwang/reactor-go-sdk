05188b7e7497b6f3ac5a7b53ca5ef609dbb517cb
wangweiwen <wangweiwen@bianfeng.com>
2020-08-03 15:15:44 +0800

    fix

--- example/main.go
+++ example/main.go
@@ -3,48 +3,66 @@ package main
 import (
 	"fmt"
 	"github.com/weiwenwang/reactor-go-sdk/reactor"
+	"log"
 	"time"
 )
 
+func DealErrChan(err_chan <-chan string) {
+	count := 0
+	for {
+		err_msg, ok := <-err_chan
+		if ok {
+			fmt.Println("err_msg: ", err_msg)
+		}
+		count++
+		if count > 20 {
+			count = 0
+			time.Sleep(5)
+		}
+	}
+}
+
 func main() {
 	// 区别正式环境和测试环境就是通过域名
-	conf := reactor.Conf{App_id: "1109861474", Secret_id: "955e76838210f9427acc6a4bc88b1a22",
+	conf := reactor.Conf{
+		App_id:     "1109861474",
+		Secret_id:  "955e76838210f9427acc6a4bc88b1a22",
 		Batch_size: 10,
-		Test_url:   "https://reactor-test.xingye.work", Prod_url: "https://reactor.xingye.work",
-		Err_chan: make(chan string, 1000),
+		Test_url:   "https://reactor-test.xingye.work",
+		Prod_url:   "https://reactor.xingye.work",
 	}
-	go func() {
-		for {
-			a, ok := <-conf.Err_chan
-			if ok {
-				fmt.Println(a)
-			}
-		}
-	}()
-	consumer, err := reactor.NewBatchConsumerWithBatchSize(&conf, reactor.ENV_TEST)
-	if err != nil {
-		panic(err.Error())
-	}
-	reactor.New(consumer)
 
-	r, _ := reactor.GetReactor()
+	consumer, err_chan, err := reactor.NewBatchConsumerWithBatchSize(&conf, reactor.ENV_TEST)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	// 开一个routine处理错误
+	go DealErrChan(err_chan)
+	reactor.New(consumer)
+	r, err := reactor.GetReactor()
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
 	for i := 0; i < 10000; i++ {
 		time.Sleep(1 * time.Second)
 		err := r.EventAdd(&reactor.Event{UserId: "123", EventId: "wang_test"})
 		if err != nil {
-			panic(err.Error())
+			log.Println("event add:", err.Error())
 		}
 
 		err2 := r.LoginAdd(&reactor.Login{UserId: "123", ChannelId: "channel1", IsNew: "1"})
 		if err2 != nil {
-			panic(err2.Error())
+			log.Println("login add:", err2.Error())
 		}
+
 		err3 := r.BattleAdd(&reactor.Battle{UserId: "123", BattleId: "123"})
 		if err3 != nil {
-			panic(err3.Error())
+			log.Println("battle add:", err3.Error())
 		}
 	}
 	r.Flush() // 这个是把当前的数据全部都发送
-	r.Close() //
+	r.Close() // close里面会调用一次flush, 您可以不需要单独调用flush
 
 }
--- reactor/consumer_batch.go
+++ reactor/consumer_batch.go
@@ -17,17 +17,58 @@ import (
 )
 
 type BatchConsumer struct {
-	serverUrl string            // 接收端地址
-	AppId     string            // 项目 APP ID
-	SecretId  string            // 项目 SecretId
-	Timeout   time.Duration     // 网络请求超时时间, 单位毫秒
-	compress  bool              //是否数据压缩
-	event_ch  chan *Event_Plus  // 数据传输信道
-	login_ch  chan *Login_Plus  // 数据传输信道
-	battle_ch chan *Battle_Plus // 数据传输信道
-	is_flush  chan struct{}
-	wg        sync.WaitGroup
-	err_chan  chan string
+	serverUrl   string            // 接收端地址
+	appId       string            // 项目 APP ID
+	secretId    string            // 项目 SecretId
+	timeout     time.Duration     // 网络请求超时时间, 单位毫秒
+	compress    bool              //是否数据压缩
+	event_ch    chan *Event_Plus  // 数据传输信道
+	login_ch    chan *Login_Plus  // 数据传输信道
+	battle_ch   chan *Battle_Plus // 数据传输信道
+	is_flush    chan struct{}
+	wg          sync.WaitGroup
+	err_chan    chan string
+	msg_chan    chan *Msg // 发送消息队列
+	re_msg_chan chan *Msg // 再次尝试的队列
+}
+
+type Msg struct {
+	Content []byte
+	Path    string
+}
+
+func (c *BatchConsumer) PreSendMsg(msg *Msg) {
+	if len(c.msg_chan) > ERR_CHANNEL_THRESHOLD_SIZE {
+		for i := 0; i < len(c.msg_chan)-ERR_CHANNEL_THRESHOLD_SIZE; i++ {
+			<-c.msg_chan
+		}
+	}
+	c.msg_chan <- msg
+}
+
+func (c *BatchConsumer) SendMsg() {
+	for {
+		rec, ok := <-c.msg_chan
+		if ok {
+			err := c.send(rec.Path, string(rec.Content))
+			if err != nil {
+				c.re_msg_chan <- rec
+			}
+		}
+	}
+}
+
+func (c *BatchConsumer) ReSendMsg() {
+	// 超过这个值就丢弃
+	for {
+		rec, ok := <-c.re_msg_chan
+		if ok {
+			err := c.send(rec.Path, string(rec.Content))
+			if err != nil {
+				c.err_chan <- err.Error() + "_content: " + string(rec.Content)
+			}
+		}
+	}
 }
 
 func (c *BatchConsumer) ReceiveErrMsg(msg string) {
@@ -57,7 +98,6 @@ type Conf struct {
 	App_id     string
 	Secret_id  string
 	Batch_size int // 每一次请求带过去多少条数据
-	Err_chan   chan string
 }
 
 type ENV int32
@@ -67,7 +107,7 @@ const (
 	ENV_PROD ENV = 1
 )
 
-func NewBatchConsumerWithBatchSize(conf *Conf, env ENV) (Consumer, error) {
+func NewBatchConsumerWithBatchSize(conf *Conf, env ENV) (Consumer, chan string, error) {
 	var url string
 	if env == ENV_PROD {
 		url = conf.Prod_url
@@ -77,146 +117,52 @@ func NewBatchConsumerWithBatchSize(conf *Conf, env ENV) (Consumer, error) {
 	if url[len(url)-1:] != "/" {
 		url = url + "/"
 	}
-	if cap(conf.Err_chan) < 1000 {
-		return nil, errors.New("Err_chan 长度必须>=1000")
-	}
+
 	return initBatchConsumer(url, conf, DEFAULT_TIME_OUT, DEFAULT_COMPRESS)
 }
 
-func initBatchConsumer(serverUrl string, conf *Conf, timeout int, compress bool) (Consumer, error) {
+func initBatchConsumer(serverUrl string, conf *Conf, timeout int, compress bool) (Consumer, chan string, error) {
 	u, err := url.Parse(serverUrl)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	if conf.Batch_size > MAX_BATCH_SIZE {
 		conf.Batch_size = MAX_BATCH_SIZE
 	}
+	err_chan := make(chan string, 1000)
 	c := &BatchConsumer{
-		serverUrl: u.String(),
-		AppId:     conf.App_id,
-		SecretId:  conf.Secret_id,
-		Timeout:   time.Duration(timeout) * time.Millisecond,
-		compress:  compress,
-		is_flush:  make(chan struct{}, 10),
-		event_ch:  make(chan *Event_Plus, BATCH_CHANNEL_SIZE),
-		login_ch:  make(chan *Login_Plus, BATCH_CHANNEL_SIZE),
-		battle_ch: make(chan *Battle_Plus, BATCH_CHANNEL_SIZE),
-		err_chan:  conf.Err_chan,
+		serverUrl:   u.String(),
+		appId:       conf.App_id,
+		secretId:    conf.Secret_id,
+		timeout:     time.Duration(timeout) * time.Millisecond,
+		compress:    compress,
+		is_flush:    make(chan struct{}, 10),
+		event_ch:    make(chan *Event_Plus, BATCH_CHANNEL_SIZE),
+		login_ch:    make(chan *Login_Plus, BATCH_CHANNEL_SIZE),
+		battle_ch:   make(chan *Battle_Plus, BATCH_CHANNEL_SIZE),
+		err_chan:    err_chan,
+		msg_chan:    make(chan *Msg, BATCH_CHANNEL_SIZE),
+		re_msg_chan: make(chan *Msg, BATCH_CHANNEL_SIZE),
 	}
+
+	//event_ch    chan *Event_Plus  // 数据传输信道
+	//login_ch    chan *Login_Plus  // 数据传输信道
+	//battle_ch   chan *Battle_Plus // 数据传输信道
+	//is_flush    chan struct{}
+	//wg          sync.WaitGroup
+	//err_chan    chan string
+	//msg_chan    chan *Msg // 发送消息队列
+	//re_msg_chan chan *Msg // 再次尝试的队列
+
 	c.wg.Add(3)
-	go DealEvent(c, conf.Batch_size, "statistic/event")
-	go DealLogin(c, conf.Batch_size, "statistic/login")
-	go DealBattle(c, conf.Batch_size, "statistic/battle")
 
-	return c, nil
+	go c.SendMsg()
+	go c.ReSendMsg()
+	go DealMsg(c, conf.Batch_size)
+	return c, err_chan, nil
 }
-func DealLogin(c *BatchConsumer, batchSize int, path string) {
-	defer func() {
-		fmt.Println("login deal done")
-		c.wg.Done()
-	}()
-	buffer := make([]*Login_Plus, 0, batchSize)
-	tiker := time.NewTicker(time.Second * time.Duration(TIKER_TIME))
-	is_stop := false // 是否停止
-	for {
-		flush := false // 是否发送
-		select {
-		case rec, ok := <-c.login_ch:
-			c.ReceiveErrMsg("Login get one msg")
-			if !ok {
-				flush = true
-				is_stop = true
-				break
-			}
-			buffer = append(buffer, rec)
-			if len(buffer) > batchSize {
-				flush = true
-			}
-			// 超过阈值的时候, 丢掉老数据
-			if len(buffer) > BATCH_CHANNEL_THRESHOLD_SIZE {
-				for i := 0; i < len(buffer)-BATCH_CHANNEL_THRESHOLD_SIZE; i++ {
-					<-c.event_ch
-				}
-			}
-		case <-tiker.C:
-			flush = true
 
-		case <-c.is_flush:
-			flush = true
-		}
-		// 上传数据到服务端, 不会重试
-		if flush {
-			if len(buffer) > 0 { // 有数据才发送
-				jdata, err := json.Marshal(buffer)
-				if err == nil {
-					err = c.send(path, string(jdata))
-					if err != nil {
-						c.err_chan <- err.Error()
-					}
-				}
-				buffer = buffer[:0]
-				flush = false
-			}
-		}
-		if is_stop == true {
-			break
-		}
-	}
-}
-func DealBattle(c *BatchConsumer, batchSize int, path string) {
-	defer func() {
-		fmt.Println("battle deal done")
-		c.wg.Done()
-	}()
-	buffer := make([]*Battle_Plus, 0, batchSize)
-	tiker := time.NewTicker(time.Second * time.Duration(TIKER_TIME))
-	is_stop := false // 是否停止
-	for {
-		flush := false // 是否发送
-		select {
-		case rec, ok := <-c.battle_ch:
-			c.ReceiveErrMsg("Battle get one msg")
-			if !ok {
-				flush = true
-				is_stop = true
-				break
-			}
-			buffer = append(buffer, rec)
-			if len(buffer) > batchSize {
-				flush = true
-			}
-			// 超过阈值的时候, 丢掉老数据
-			if len(buffer) > BATCH_CHANNEL_THRESHOLD_SIZE {
-				for i := 0; i < len(buffer)-BATCH_CHANNEL_THRESHOLD_SIZE; i++ {
-					<-c.event_ch
-				}
-			}
-		case <-tiker.C:
-			flush = true
-
-		case <-c.is_flush:
-			flush = true
-		}
-		// 上传数据到服务端, 不会重试
-		if flush {
-			if len(buffer) > 0 { // 有数据才发送
-				jdata, err := json.Marshal(buffer)
-				if err == nil {
-					err = c.send(path, string(jdata))
-					if err != nil {
-						fmt.Println(err)
-					}
-				}
-				buffer = buffer[:0]
-				flush = false
-			}
-		}
-		if is_stop == true {
-			break
-		}
-	}
-}
 func DealEvent(c *BatchConsumer, batchSize int, path string) {
 	defer func() {
 		fmt.Println("event deal done")
@@ -273,18 +219,133 @@ func DealEvent(c *BatchConsumer, batchSize int, path string) {
 	}
 }
 
+func DealMsg(c *BatchConsumer, batchSize int) {
+	defer func() {
+		fmt.Println("event deal done")
+		c.wg.Done()
+	}()
+	buffer_event := make([]*Event_Plus, 0, batchSize)
+	buffer_login := make([]*Login_Plus, 0, batchSize)
+	buffer_battle := make([]*Battle_Plus, 0, batchSize)
+	tiker := time.NewTicker(time.Second * time.Duration(TIKER_TIME))
+	is_stop := false // 是否停止
+	for {
+		flush_event := false  // 是否发送
+		flush_login := false  // 是否发送
+		flush_battle := false // 是否发送
+		select {
+		case rec, ok := <-c.event_ch:
+			c.ReceiveErrMsg("Event get on msg")
+			// 关闭了
+			if !ok {
+				flush_event = true
+				is_stop = true
+				break
+			}
+			buffer_event = append(buffer_event, rec)
+		case rec, ok := <-c.login_ch:
+			c.ReceiveErrMsg("login get on msg")
+			// 关闭了
+			if !ok {
+				flush_login = true
+				is_stop = true
+				break
+			}
+			buffer_login = append(buffer_login, rec)
+
+		case rec, ok := <-c.battle_ch:
+			c.ReceiveErrMsg("battle get on msg")
+			// 关闭了
+			if !ok {
+				flush_battle = true
+				is_stop = true
+				break
+			}
+			buffer_battle = append(buffer_battle, rec)
+
+		case <-tiker.C:
+			flush_event = true
+			flush_login = true
+			flush_battle = true
+
+		case <-c.is_flush:
+			flush_event = true
+			flush_login = true
+			flush_battle = true
+		}
+
+		// buf满了, 或者flush_event = true 都要发送
+		if len(buffer_event) > batchSize || flush_event {
+			if len(buffer_event) > 0 { // flush_event = true 但是又没有数据的时候, 不发送
+				jdata, err := json.Marshal(buffer_event)
+				if err != nil {
+					c.err_chan <- "json encode login msg error " + err.Error()
+				} else {
+					msg := Msg{
+						Content: jdata,
+						Path:    "statistic/event",
+					}
+					c.PreSendMsg(&msg)
+					buffer_event = buffer_event[:0]
+				}
+			}
+			flush_event = false
+		}
+
+		// buf满了, 或者flush_event = true 都要发送
+		if len(buffer_login) > batchSize || flush_login {
+			if len(buffer_login) > 0 { // flush_* = true 但是又没有数据的时候, 不发送
+				jdata, err := json.Marshal(buffer_login)
+				if err != nil {
+					c.err_chan <- "json encode login msg error " + err.Error()
+				} else {
+					msg := Msg{
+						Content: jdata,
+						Path:    "statistic/login",
+					}
+					c.PreSendMsg(&msg)
+					buffer_login = buffer_login[:0]
+				}
+			}
+			flush_login = false
+		}
+
+		// buf满了, 或者flush_event = true 都要发送
+		if len(buffer_battle) > batchSize || flush_battle {
+			if len(buffer_battle) > 0 { // flush_* = true 但是又没有数据的时候, 不发送
+				jdata, err := json.Marshal(buffer_battle)
+				if err != nil {
+					c.err_chan <- "json encode login msg error " + err.Error()
+				} else {
+					msg := Msg{
+						Content: jdata,
+						Path:    "statistic/battle",
+					}
+					c.PreSendMsg(&msg)
+					buffer_battle = buffer_battle[:0]
+				}
+			}
+			flush_battle = false
+		}
+
+		if is_stop == true {
+			break
+		}
+	}
+}
+
 func (c *BatchConsumer) AddEvent(d *Event) error {
-	c.event_ch <- &Event_Plus{*d, c.AppId, Md5V(c.SecretId + "_" + c.AppId + "_" + d.DataTime)}
+	c.event_ch <- &Event_Plus{*d, c.appId, Md5V(c.secretId + "_" + c.appId + "_" + d.DataTime)}
 	return nil
 }
 
 func (c *BatchConsumer) AddLogin(d *Login) error {
-	c.login_ch <- &Login_Plus{*d, c.AppId, Md5V(c.SecretId + "_" + c.AppId + "_" + d.DataTime)}
+	c.login_ch <- &Login_Plus{*d, c.appId, Md5V(c.secretId + "_" + c.appId + "_" + d.DataTime)}
 	return nil
 }
 
 func (c *BatchConsumer) AddBattle(d *Battle) error {
-	c.battle_ch <- &Battle_Plus{*d, c.AppId, Md5V(c.SecretId + "_" + c.AppId + "_" + d.DataTime)}
+	c.battle_ch <- &Battle_Plus{*d, c.appId, Md5V(c.secretId + "_" + c.appId + "_" + d.DataTime)}
 	return nil
 }
 
@@ -324,7 +385,7 @@ func (c *BatchConsumer) send(path string, data string) error {
 	req.Header.Set("user-agent", "reactor-go-sdk")
 	req.Header.Set("version", "1.1.0")
 	req.Header.Set("compress", compressType)
-	client := &http.Client{Timeout: c.Timeout}
+	client := &http.Client{Timeout: c.timeout}
 	resp, err = client.Do(req)
 
 	if err != nil {
@@ -338,12 +399,10 @@ func (c *BatchConsumer) send(path string, data string) error {
 		var result struct {
 			Code int
 		}
-
 		err = json.Unmarshal(body, &result)
 		if err != nil {
 			return err
 		}
-
 		if result.Code != 0 {
 			fmt.Println(string(body))
 			return errors.New(fmt.Sprintf("send to receiver failed with return code: %d", result.Code))
